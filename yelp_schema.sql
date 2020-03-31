a8f89b1b13ad610d129a84cdf825cf53156e02ee
Tarek98 <tabdelme@edu.uwaterloo.ca>
2020-03-31 00:05:43 +0000

    start yelp table creation

---
+++ yelp_schema.sql
@@ -0,0 +1,53 @@
+create database YELP_DB;
+
+use baseball_test;
+set NAMES utf8mb4;
+
+drop table if exists `Business`;
+create table `Business` (
+    `business_id` varchar(255) default null,
+    `name` varchar(255) default null,
+    `neighborhood` varchar(255) default null,
+    `address` varchar(255) default null,
+    `city` varchar(255) default null,
+    `state` varchar(2) default null,
+    `postal_code` varchar(10) default null,
+    `latitude` decimal(20, 18) default null,
+    `longitude` decimal(20, 17) default null,
+    `stars` decimal(2,1) default null,
+    `review_count` int default null,
+    `is_open` int default null,
+    `categories` varchar(255) default null
+);
+
+drop table if exists `Checkin`;
+create table `Checkin` (
+    `business_id` varchar(255) default null,
+    `weekday` varchar(3) default null,
+    `hour` varchar(5) default null,
+    `checkins` int default null
+);
+
+drop table if exists `User`;
+create table `User` (
+    `user_id` varchar(255) default null,
+    `name` varchar(255) default null,
+    `review_count` int default null,
+    `yelping_since` varchar(10) default null,
+    `friends` varchar(5000) default null,
+    `useful` int default null,
+    `funny` int default null,
+    `cool` int default null,
+    `fans` int default null,
+    `elite` varchar(255) default null,
+    `average_stars` decimal(3,2) default null,
+    `last_online` varchar(10) default null
+);
+
+drop table if exists `Review`;
+create table `Review` (
+    `review_id` varchar(255) default null,
+    `user_id` varchar(3) default null,
+    `` varchar(5) default null,
+    `` int default null
+);
