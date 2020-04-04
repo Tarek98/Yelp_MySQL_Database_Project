684a51d61d29f53c1c3b1204e1456e096cf6be5a
Lefteris Spetsakis <lspetsak@edu.uwaterloo.ca>
2020-04-04 14:26:22 -0400

    Added api

---
+++ yelp_api.py
@@ -0,0 +1,33 @@
+
+
+class YelpServer(object):
+    def __init__(self):
+        pass
+
+    def post_on_topic(self, topic):
+        pass
+
+    def follow_user(self, user):
+        pass
+
+    def follow_topic(self, topic):
+        pass
+
+    def get_latest_posts(self):
+        pass
+
+    def react_to_post(self, post):
+        pass
+
+    def get_post(self, user, date):
+        pass
+
+    def get_all_topics(self):
+        pass
+
+    def get_all_users(self):
+        pass
+
+    def search_user(self, firstname, lastname):
+        pass
+    
