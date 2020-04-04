7691f65efa75b19c0112e6b0ae16ab327fc23330
Tarek98 <tabdelme@edu.uwaterloo.ca>
2020-04-04 18:28:48 +0000

    fix user of mysql connector and add comments

--- load_data_remote.py
+++ load_data_remote.py
@@ -2,7 +2,9 @@ import mysql.connector
 from mysql.connector import errorcode
 
 try:
-    cnx = mysql.connector.connect(user="root", password="user_356",
+    # replace user and password with credentials for mysql shell user with YELP_DB full access
+    # replace host with VM IP address (VM network setup as bridged adapter from virtualbox)
+    cnx = mysql.connector.connect(user="user_356", password="user_356",
                                     host='192.168.2.208', database='YELP_DB')
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
